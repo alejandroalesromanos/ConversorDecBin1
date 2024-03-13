4b783f0976ccea85b17ac7753b93c7f74ef32879
ikasle <ikasle@DAM1-01.arangoya.local>
2024-03-13 12:19:34 +0100

    Vista Actualizada

--- ConersorDecBin/src/vista/Conversor.java
+++ ConersorDecBin/src/vista/Conversor.java
@@ -5,11 +5,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.JLabel;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class Conversor extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField decimal;
 
 	/**
 	 * Launch the application.
@@ -31,12 +40,78 @@ public class Conversor extends JFrame {
 	 * Create the frame.
 	 */
 	public Conversor() {
+		setTitle("Conversor Decimal a Binario");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{10, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JPanel panel = new JPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.anchor = GridBagConstraints.SOUTH;
+		gbc_panel.gridwidth = 2;
+		gbc_panel.insets = new Insets(0, 0, 5, 5);
+		gbc_panel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panel.gridx = 1;
+		gbc_panel.gridy = 0;
+		contentPane.add(panel, gbc_panel);
+		
+		JLabel lblNewLabel = new JLabel("Decimal");
+		panel.add(lblNewLabel);
+		
+		JPanel panel_1 = new JPanel();
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.gridwidth = 2;
+		gbc_panel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_panel_1.fill = GridBagConstraints.BOTH;
+		gbc_panel_1.gridx = 1;
+		gbc_panel_1.gridy = 1;
+		contentPane.add(panel_1, gbc_panel_1);
+		
+		decimal = new JTextField();
+		panel_1.add(decimal);
+		decimal.setColumns(10);
+		
+		JPanel panel_2 = new JPanel();
+		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
+		gbc_panel_2.anchor = GridBagConstraints.SOUTH;
+		gbc_panel_2.gridwidth = 2;
+		gbc_panel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_panel_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panel_2.gridx = 1;
+		gbc_panel_2.gridy = 2;
+		contentPane.add(panel_2, gbc_panel_2);
+		
+		JLabel lblNewLabel_1 = new JLabel("Binario");
+		panel_2.add(lblNewLabel_1);
+		
+		JPanel panel_3 = new JPanel();
+		GridBagConstraints gbc_panel_3 = new GridBagConstraints();
+		gbc_panel_3.gridwidth = 2;
+		gbc_panel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_panel_3.fill = GridBagConstraints.BOTH;
+		gbc_panel_3.gridx = 1;
+		gbc_panel_3.gridy = 3;
+		contentPane.add(panel_3, gbc_panel_3);
+		
+		JLabel binario = new JLabel("");
+		panel_3.add(binario);
+		
+		JButton botonCalcular = new JButton("Calcular");
+		GridBagConstraints gbc_botonCalcular = new GridBagConstraints();
+		gbc_botonCalcular.gridwidth = 2;
+		gbc_botonCalcular.insets = new Insets(0, 0, 0, 5);
+		gbc_botonCalcular.gridx = 1;
+		gbc_botonCalcular.gridy = 4;
+		contentPane.add(botonCalcular, gbc_botonCalcular);
 	}
 
 }
