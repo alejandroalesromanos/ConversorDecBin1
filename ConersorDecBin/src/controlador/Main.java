bbd7ae5f6493974ceebc12fb58d66cd317e3aefa
ikasle <ikasle@DAM1-14.arangoya.local>
2024-03-13 12:26:40 +0100

    Lógica del proyecto realizada

--- ConersorDecBin/src/controlador/Main.java
+++ ConersorDecBin/src/controlador/Main.java
@@ -1,10 +1,13 @@
 package controlador;
 
+import modelo.Numero;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Numero numero = new Numero();
+		System.out.println(numero.convertirBinario(10));;
+		
 	}
 
 }
--- ConersorDecBin/src/modelo/Numero.java
+++ ConersorDecBin/src/modelo/Numero.java
@@ -1,5 +1,23 @@
 package modelo;
 
 public class Numero {
+    private int numDecimal;
+    private int numBinario;
+    
+    public Numero() {
+    }
+
+    public String convertirBinario(int decimal) {
+        String binario = "";
+        
+        while (decimal > 0) {
+            int resto = decimal % 2;
+            binario = resto + binario;
+            decimal = decimal / 2;
+        }
+        
+        return binario;
+    }
+   
 
 }
